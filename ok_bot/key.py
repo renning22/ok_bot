57b0242954df713e73ab6a46e45608ed3650dfe4
Ning Ren <renning22@gmail.com>
2018-12-12 06:35:59 -0800

    fix review

--- ok_bot/key.py
+++ ok_bot/key.py
@@ -2,7 +2,7 @@ import os
 
 SOURCE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(SOURCE_DIR, '..', 'key.txt')) as file:
-    api = file.read().strip()
-with open(os.path.join(SOURCE_DIR, '..', 'secret.txt')) as file:
-    secret = file.read().strip()
+with open(os.path.join(SOURCE_DIR, '..', 'key.txt')) as key_file:
+    api = key_file.read().strip()
+with open(os.path.join(SOURCE_DIR, '..', 'secret.txt')) as secret_file:
+    secret = secret_file.read().strip()
