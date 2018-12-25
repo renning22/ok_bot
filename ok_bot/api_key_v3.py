b75cf6b53e493e0c7f781fb77960ee0155896822
Ning Ren <renning22@gmail.com>
2018-12-25 04:44:33 -0800

    V3 websocket API
    python -m ok_bot.ws_reader
    
    Tested futures_depth5.
    
    TODO: test with order and position.

--- ok_bot/api_key_v3.py
+++ ok_bot/api_key_v3.py
@@ -1,7 +1,3 @@
-import os
-
-SOURCE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-API_KEY = open(f'{SOURCE_DIR}/api_key_v3').read().strip()
-KEY_SECRET = open(f'{SOURCE_DIR}/secret_key_v3').read().strip()
-PASS_PHRASE = open(f'{SOURCE_DIR}/pass_phrase_v3').read().strip()
+API_KEY = open('api_key_v3').read().strip()
+KEY_SECRET = open('secret_key_v3').read().strip()
+PASSPHRASE = open('pass_phrase_v3').read().strip()
