0ed20e269672a2e6a4d762ebdf2fe52cd03e351c
Jerry Luan <jerry.luan@airbnb.com>
2019-01-04 10:02:26 -0800

    fix api_v3_key_reader.py

--- ok_bot/api_v3_key_reader.py
+++ ok_bot/api_v3_key_reader.py
@@ -1,3 +1,14 @@
-API_KEY = open('api_key_v3').read().strip()
-KEY_SECRET = open('secret_key_v3').read().strip()
-PASSPHRASE = open('pass_phrase_v3').read().strip()
+import os
+
+SOURCE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+API_KEY = None
+KEY_SECRET = None
+PASS_PHRASE = None
+
+with open(os.path.join(SOURCE_DIR, 'api_key_v3')) as key_file:
+    API_KEY = key_file.read().strip()
+with open(os.path.join(SOURCE_DIR, 'secret_key_v3')) as secret_file:
+    KEY_SECRET = secret_file.read().strip()
+with open(os.path.join(SOURCE_DIR, 'pass_phrase_v3')) as secret_file:
+    PASS_PHRASE = secret_file.read().strip()
