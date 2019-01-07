fc6e38d5bbc83e571ef090a4355cca57c635e238
Jerry Luan <jerry.luan@airbnb.com>
2019-01-06 21:38:31 -0800

    [refact] add BookListener

--- ok_bot/book_listener.py
+++ ok_bot/book_listener.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from collections import defaultdict
 
 import eventlet
 from absl import app, logging
@@ -7,12 +8,27 @@ from absl import app, logging
 class BookListener:
     def __init__(self):
         logging.info('BookListener initiated')
+        self.subscribers = defaultdict(set)
 
-    def subscribe(self, instrument_id, responsor):
-        pass
+    def subscribe(self, instrument_id, responder):
+        '''
+        :param instrument_id:
+        :param responder: responder need to implement
+                      tick_received(instrument_id,
+                                    ask_prices,
+                                    ask_vols,
+                                    bid_prices,
+                                    bid_vols,
+                                    timestamp)
+        :return: None
+        '''
+        if not hasattr(responder, 'tick_received'):
+            raise Exception(f"{type(responder)} doesn't have tick_received method")
+        self.subscribers[instrument_id].add(responder)
 
-    def unsubscribe(self, ):
-        pass
+    def unsubscribe(self, instrument_id, responder):
+        if responder in self.subscribers[instrument_id]:
+            self.subscribers.remove(responder)
 
     def received_futures_depth5(self,
                                 asks,
@@ -25,30 +41,40 @@ class BookListener:
         ask_vols = [int(i[1]) for i in asks]
         bid_prices = [Decimal(str(i[0])) for i in bids]
         bid_vols = [int(i[1]) for i in bids]
-        self.order_book.update_book(
-            instrument_id, ask_prices, ask_vols, bid_prices, bid_vols)
+        for responder in self.subscribers[instrument_id]:
+            responder.tick_received(instrument_id,
+                                    ask_prices,
+                                    ask_vols,
+                                    bid_prices,
+                                    bid_vols,
+                                    timestamp)
 
 
 def _testing(_):
-    from .order_book import OrderBook
     from .websocket_api import WebsocketApi
     from .schema import Schema
 
     class MockTrader:
-        def new_tick_received(self, order_book):
-            logging.info('mock trader got new tick: %d, %s',
-                         order_book.row_num,
-                         order_book.recent_tick_source())
+        def tick_received(self,
+                          instrument_id,
+                          ask_prices,
+                          ask_vols,
+                          bid_prices,
+                          bid_vols,
+                          timestamp):
+            logging.info('mock trader got new tick: %s, best ask: %.3f@%d,'
+                         'best bid: %.3f@%d',
+                         instrument_id,
+                         ask_prices[0], ask_vols[0],
+                         bid_prices[0], bid_vols[0])
 
     pool = eventlet.GreenPool()
-
     schema = Schema('BTC')
+    book_listener = BookListener()
     trader = MockTrader()
-    order_book = OrderBook(schema, trader)
-    reader = BookReader(order_book)
-    api = WebsocketApi(pool, reader, schema)
+    book_listener.subscribe('BTC-USD-190118', trader)
+    api = WebsocketApi(pool, book_listener, schema)
     api.start_read_loop()
-
     pool.waitall()
 
 
