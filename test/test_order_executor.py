7d59acc8da66cf8e131149a4d74d794caa6a0ad8
Ning Ren <renning22@gmail.com>
2019-01-21 18:10:29 -0800

    make test_order_executor unit test pass again

--- test/test_order_executor.py
+++ test/test_order_executor.py
@@ -1,21 +1,47 @@
+import logging
 import unittest
 from decimal import Decimal
-from unittest.mock import ANY, MagicMock
+from unittest.mock import ANY, MagicMock, patch
 
 import eventlet
 
-from ok_bot.order_executor import OrderExecutor
+from ok_bot import order_executor, singleton
+
+
+class MockOrderListerner_cancelAfterSubscribe:
+    def subscribe(self, order_id, subscriber):
+        print('subscribe')
+        eventlet.greenthread.spawn_after_local(
+            1, lambda: subscriber.order_cancelled(order_id))
+
+    def unsubscribe(self, order_id, subscriber):
+        print('unsubscribed')
 
 
 class TestOrderExecutor(unittest.TestCase):
 
     def setUp(self):
-        self._mock_api = MagicMock()
-        self._order_executor = OrderExecutor(self._mock_api)
+        singleton.green_pool = eventlet.GreenPool()
+        singleton.rest_api = MagicMock()
+        singleton.rest_api.open_long_order.__name__ = 'fake_open_long_order'
+        singleton.order_listener = MockOrderListerner_cancelAfterSubscribe()
 
-    def test_open_arbitrage_position(self):
-        self._order_executor.open_arbitrage_position(
-            'this_week', 30, 'next_week', 300, 1)
+    def test_order_cancelled(self):
+        def _testing_thread():
+            executor = order_executor.OrderExecutor('instrument_id',
+                                                    amount=1,
+                                                    price=100.0,
+                                                    timeout_sec=20,
+                                                    is_market_order=False,
+                                                    logger=logging)
+            order_status_future = executor.open_long_position()
+            print('open_long_position has been called')
+            result = order_status_future.get()
+            print(f'result: {result}')
+            assert result is order_executor.OPEN_POSITION_STATUS__CANCELLED
+
+        testing_thread = singleton.green_pool.spawn(_testing_thread)
+        testing_thread.wait()
 
 
 if __name__ == '__main__':
