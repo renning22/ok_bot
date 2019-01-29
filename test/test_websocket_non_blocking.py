8417a98da67217542898eb5a52714527abff79e5
Ning Ren <renning22@gmail.com>
2019-01-28 17:42:27 -0800

    test_websocket_non_blocking working

--- test/test_websocket_non_blocking.py
+++ test/test_websocket_non_blocking.py
@@ -1,12 +1,13 @@
+import asyncio
 import pprint
 import unittest
+from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 
-import eventlet
+import requests
 from absl import logging
 
-import greenlet
-from ok_bot import patched_io_modules, singleton
+from ok_bot import singleton
 
 _URL = 'http://www.google.com'
 _CRAWL_TIMES = 10
@@ -17,46 +18,48 @@ class TestWebsocketNonblocking(unittest.TestCase):
     def setUp(self):
         logging.get_absl_logger().setLevel(logging.DEBUG)
 
-    def test_identical(self):
-        requests = eventlet.import_patched('requests')
-        self.assertIs(requests, patched_io_modules.requests)
-
     def test_non_blocking(self):
-        def crawl_job():
+        async def crawl_job():
             crawled_times = 0
-            requests = eventlet.import_patched('requests')
             for _ in range(_CRAWL_TIMES):
-                resp = requests.get(_URL)
-                logging.info(f'get %s from %s', resp.status_code, _URL)
-                eventlet.sleep(1)
+                with ThreadPoolExecutor(max_workers=1) as executor:
+                    resp = await singleton.loop.run_in_executor(
+                        executor, requests.get, _URL)
+                logging.info('get %s from %s', resp.status_code, _URL)
+                await asyncio.sleep(1)
                 crawled_times += 1
             logging.info('all crawl finished')
             return crawled_times
+
+        async def sleeper():
+            await asyncio.sleep(_CRAWL_TEST_TIMEOUT_SEC)
+
+        async def _testing_coroutine(test_class):
+            with self.assertLogs(logging.get_absl_logger(), level='INFO') as cm:
+                t = await asyncio.gather(crawl_job(), sleeper())
+                crawled_times = t[0]
+                log_message = cm.output
+
+            print(pprint.pformat(log_message))
+            test_class.assertIn(
+                'INFO:absl:Sending heartbeat message', log_message)
+            test_class.assertIn(
+                'INFO:absl:Received heartbeat message', log_message)
+            test_class.assertEqual(
+                crawled_times, _CRAWL_TIMES,
+                f'crawled {crawled_times} expected {_CRAWL_TIMES}')
+            test_class.assertGreaterEqual(
+                singleton.websocket.heartbeat_ping, 1,
+                f'{singleton.websocket.heartbeat_ping}')
+            test_class.assertGreaterEqual(
+                singleton.websocket.heartbeat_pong, 1,
+                f'{singleton.websocket.heartbeat_pong}')
 
         singleton.initialize_objects_with_mock_trader_and_dev_db('ETH')
         # make sure nothing will return from websocket subscription
         singleton.websocket.book_listener = None
         singleton.websocket.start_read_loop()
-
-        crawled_job_thread = eventlet.spawn(crawl_job)
-
-        log_message = None
-        with self.assertLogs(logging.get_absl_logger(), level='INFO') as cm:
-            eventlet.sleep(_CRAWL_TEST_TIMEOUT_SEC)
-            log_message = cm.output
-
-        crawled_times = crawled_job_thread.wait()
-
-        print(pprint.pformat(log_message))
-        self.assertIn('INFO:absl:Sending heartbeat message', log_message)
-        self.assertIn(
-            'INFO:absl:Received heartbeat message', log_message)
-        self.assertEqual(crawled_times, _CRAWL_TIMES,
-                         f'crawled {crawled_times} expected {_CRAWL_TIMES}')
-        self.assertGreaterEqual(singleton.websocket.heartbeat_ping, 1,
-                                f'{singleton.websocket.heartbeat_ping}')
-        self.assertGreaterEqual(singleton.websocket.heartbeat_pong, 1,
-                                f'{singleton.websocket.heartbeat_pong}')
+        singleton.loop.run_until_complete(_testing_coroutine(self))
 
 
 if __name__ == '__main__':
