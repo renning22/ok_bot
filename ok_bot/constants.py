13f9efc0d450b06476b09692ca25cbc6f3650c85
Ning Ren <renning22@gmail.com>
2019-02-06 18:26:07 -0800

    adjust simple params

--- ok_bot/constants.py
+++ ok_bot/constants.py
@@ -13,14 +13,14 @@ INSUFFICIENT_MARGIN_COOL_DOWN_SECOND = 60 * 10  # 10 minutes
 
 # Unit: USD per contract per transaction.
 # -0.001 means we expect losing 0.1 cent per transaction.
-SIMPLE_STRATEGY_NET_PROFIT_THRESHOLD = -0.001
+SIMPLE_STRATEGY_NET_PROFIT_THRESHOLD = 0
 
 # X >= 1 (standard deviation) is 84% percentiles in standard gaussian
 # distribution, 34% deviated from center.
 SIMPLE_STRATEGY_ZSCORE_THRESHOLD = 1.0
 
 # Bounds back distance.
-SIMPLE_STRATEGY_RESILIANCE = 0.75
+SIMPLE_STRATEGY_RESILIANCE = 0.60
 
 # Arbitrage
 # According to https://www.okex.com/pages/products/fees.html, for Lv1
